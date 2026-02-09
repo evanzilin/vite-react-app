251b37a833d3e2d272ea2a6a6e9a7dbe2e4e5b57
CodeStriker-thunder <acakeksup@gmail.com>
2026-02-09 09:11:19 -0800

    chore: delete gap

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -320,7 +320,7 @@ function SidebarFooter({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="sidebar-footer"
       data-sidebar="footer"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn("flex flex-col gap-2", className)}
       {...props}
     />
   )
@@ -359,7 +359,7 @@ function SidebarGroup({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="sidebar-group"
       data-sidebar="group"
-      className={cn("relative flex w-full min-w-0 flex-col", className)}
+      className={cn("relative flex w-full min-w-0 flex-col p-2", className)}
       {...props}
     />
   )
